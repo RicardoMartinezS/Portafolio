1c603115fb0adc99b94b1bfae4fdaf8a0dfee75b
Ricardo Andres Martinez Soto <rmartinez@acdivoca-co.org>
2024-06-26 11:50:11 -0500

    1.1

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -18,3 +18,19 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+var swiper = new Swiper(".mySwiper-1", {
+    slidesPerView:1,
+    spaceBetween:15,
+    loop:true,
+    pagination:{
+        el:".swiper-pagination",
+        clickable:true,
+    },
+    navigation: {
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
+    }
+
+});
+
